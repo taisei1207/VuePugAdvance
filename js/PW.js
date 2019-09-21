58f7559b25669237d988c0917ac56e4a7dafc782
taisei <taisei.radio.1207@gmail.com>
2019-09-21 16:42:22 +0900

    スマホPW作成

--- js/PW.js
+++ js/PW.js
@@ -1,16 +1,28 @@
-new Vue({
+let vm = new Vue({
 	el: '#app',
 	data: {
-		nums: {
-			"num1": null,
-			"num2": null,
-			"num3": null,
-			"num4": null
-		}
+		"num1": null,
+		"num2": null,
+		"num3": null,
+		"num4": null
 	},
 	methods: {
-		goNext: function() {
+		goNext: function(event) {
 			$inputs = document.getElementsByClassName('input-box');
+			nowIndex = Array.from($inputs).indexOf(event.target);
+			var ref = 0;
+			var nextRef = 0;
+			ref = Number(event.target.id);
+			if (event.target.value.length == 1) {
+				if (nowIndex < $inputs.length - 1) {
+					nextRef = ref + 1;
+					this.$refs[nextRef].focus();
+				}
+
+				else {
+					this.$refs[ref].blur();
+				}
+			}
 		}
 	}
 })
